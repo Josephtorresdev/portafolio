dce8c14bb954eb03842f558121856775b1f4cde6
Josephtorresdev <113720204+Josephtorresdev@users.noreply.github.com>
2022-09-16 15:37:32 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,31 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.top-menu', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.titulo-bienvenida', {
+        duration: 4000,
+        origin: 'bottom',
+        distance: '-400px'
+    });
+
+     sr.reveal('.lista-bienvenida', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-400px'
+    });
+
+    sr.reveal('.descargar-curriculum', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-400px'
+    });
+    
+    sr.reveal('.main-cursos', {
+        duration: 2500,
+        origin: 'left',
+        distance: '600px'
+    });
