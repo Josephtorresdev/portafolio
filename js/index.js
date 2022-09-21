8b791fc38b1e7e7094ccca767e6cc0b50b37a539
Joseph Torres <113720204+Josephtorresdev@users.noreply.github.com>
2022-09-21 01:31:40 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -43,3 +43,20 @@ window.sr = ScrollReveal();
         distance: '600px'
     });
 
+     $(document).ready(function(){
+
+        $('.ir-arriba').click(function(){
+            $('body, html').animate({
+                scrollTop: '0px'
+            }, 600);
+        });
+    
+        $(window).scroll(function(){
+            if( $(this).scrollTop() > 0 ){
+                $('.ir-arriba').slideDown(300);
+            } else {
+                $('.ir-arriba').slideUp(300);
+            }
+        });
+    
+    });
