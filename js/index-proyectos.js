cf1d5e19784ce8ccfbdde04e19c6d61fefb4730b
Joseph Torres <113720204+Josephtorresdev@users.noreply.github.com>
2022-10-03 11:00:23 -0400

    Add files via upload

---
+++ js/index-proyectos.js
@@ -0,0 +1,29 @@
+var check=document.querySelector(".check");
+check.addEventListener('click', idioma)
+
+function idioma(){
+    let id=check.checked;
+    if (id==true){
+        location.href="en/projects.html";
+    }else{
+        location.href="../proyectos.html";
+    }
+}
+
+window.sr = ScrollReveal();
+
+    sr.reveal('.top-menu', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
---
+++ js/index.js
@@ -0,0 +1,72 @@
+
+var check=document.querySelector(".check");
+check.addEventListener('click', idioma)
+
+function idioma(){
+    let id=check.checked;
+    if (id==true){
+        location.href="en/index.html";
+    }else{
+        location.href="../index.html";
+    }
+}
+
+window.sr = ScrollReveal();
+
+    sr.reveal('.top-menu', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.titulo-bienvenida', {
+        duration: 4000,
+        origin: 'bottom',
+        distance: '-400px'
+    });
+
+     sr.reveal('.lista-bienvenida', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-400px'
+    });
+
+    sr.reveal('.descargar-curriculum', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-400px'
+    });
+    
+    sr.reveal('.main-cursos', {
+        duration: 2500,
+        origin: 'left',
+        distance: '600px'
+    });
+
+    $(document).ready(function(){
+
+        $('.ir-arriba').click(function(){
+            $('body, html').animate({
+                scrollTop: '0px'
+            }, 600);
+        });
+    
+    $(window).scroll(function(){
+        if( $(this).scrollTop() > 0 ){
+            $('.ir-arriba').slideDown(300);
+        } else {
+            $('.ir-arriba').slideUp(300);
+        }
+    }); 
+});
+
+
+        /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
